a9de3d091086b7f97d7e39cbd2b2e3b40b0fdb0f
Eduardo Oliveira <edurafael.deoliveira@gmail.com>
2023-01-06 15:48:10 -0300

    Criacao menu login

--- components/popupUser/popupUser.js
+++ components/popupUser/popupUser.js
@@ -1,17 +1,34 @@
 import React, { useState } from "react";
-import { StyleSheet, Image, TouchableOpacity, Modal, SafeAreaView } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity, Modal, SafeAreaView, View, Text } from 'react-native';
 
 //Images
 import UserIcon from '../../scr/images/icons/userIcon.png';
 
 export default function PopupUser(){
 
+    const [modalVisible, setModalVisible] = useState(false);
     
     return (
         <>
-            <TouchableOpacity  onPress={()=>{setVisible(true)}}>
+            <TouchableOpacity onPress={()=>setModalVisible(true)}>
                 <Image source={UserIcon} style={estilos.imgUserIcon} />
             </TouchableOpacity>
+            <Modal
+                visible={modalVisible}
+                animationType="fade"
+                transparent={true}
+            >
+                <SafeAreaView style={estilos.safeAreaView} onTouchStart={()=>setModalVisible(false)}>
+                    <View style={estilos.box}>
+                        <TouchableOpacity>
+                            <Text>Logar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text>Registrar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </SafeAreaView>
+            </Modal>
         </>
     );
 }
@@ -20,5 +37,16 @@ const estilos = StyleSheet.create({
     imgUserIcon: {
         width: 40,
         height: 40,
-    }
+    },
+    safeAreaView: {
+        width: "100%",
+        height: "100%",
+        alignItems: "flex-end",
+    },
+    box:{
+        width: "75%",
+        height: "100%",
+        backgroundColor: "#ff0000",
+        marginTop: "22%"
+    },
 });
